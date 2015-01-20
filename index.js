dd1c1924ba9b3ab0bb0937d4482173e53bfaa602
Stuart Wilson <stuart.wilson@graze.com>
2015-01-20 14:14:55 +0000

    Converted main module to event emitter

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+/**
+ * To do:
+ * --------------
+ *
+ * Modularise:
+ *     - Break down into single file validator and api call
+ *     - Use index.js for binding
+ *     - Add support for CLI
+ *
+ * Rename to imogen namespace
+ *
+ * Write tests
+ */
+
 var fs = require('fs');
 var findit = require('findit');
 var path = require('path');
-var chalk = require('chalk');
-var argv = require('minimist')(process.argv.slice(2));
+var argv = require('minimist')(process.argv);
+var events = require('events');
 
 var TinyPng = require('./lib/tiny-png');
 
@@ -13,6 +27,7 @@ if (args) {
     walkDir(args.dir, args.key);
 }
 
+// Pulls out arguments
 function getArgs() {
 
     if (! argv.d) {
@@ -25,21 +40,43 @@ function getArgs() {
     }
 }
 
+// Checks that the file is a .png or .jpg
 function isValidFile(file) {
     return ['.png', '.jpg'].indexOf(path.extname(file)) > -1;
 }
 
-function walkDir(dir, apiKey, callback) {
+// Walks a directory and compresses images
+function walkDir(dir, apiKey) {
+
+    // Setup api client
     var api = new TinyPng({
         key: apiKey
     });
 
+    // Time object for duration based calculations
+    var time = {
+        start: new Date(),
+        end: new Date(),
+        duration: function() {
+            return this.end.getTime() - this.start.getTime();
+        },
+        format: function(time) {
+            return Math.round(time / 1000);
+        }
+    };
+
+    // Event emitter to return
+    var event = new events.EventEmitter();
+
+    // Directory walker
     var finder = findit(dir);
 
+    // Basic file compression count
     var totalSaving = 0;
     var validFileCount = 0;
     var compressedFileCount = 0;
 
+    // Check for parameters
     if(! dir) {
         throw 'No directory set';
     }
@@ -48,28 +85,50 @@ function walkDir(dir, apiKey, callback) {
         throw 'No API key set';
     }
 
+    // Walk direcory and trigger event on find
     finder.on('file', function(file, stat) {
         if (file && isValidFile(file)) {
             api.compress(file, function(err, data) {
+
                 if (err) {
                     validFileCount --;
-                    return console.error(err);
+                    return event.emit('error', err);
                 }
 
-                console.log(
-                    'Source: %s (%s) reduced by %s% (%s)',
-                    chalk.magenta(data.input.filename),
-                    Math.round(data.input.size / 1000) + 'kb',
-                    Math.round((1 - data.output.ratio) * 100),
-                    chalk.green(Math.round(data.output.size / 1000) + 'kb')
-                );
+                event.emit('file', {
+                    input: {
+                        name: data.input.filename,
+                        size: Math.round(data.input.size / 1000),
+                        unit: 'kb'
+                    },
+                    output: {
+                        name: data.output.filename,
+                        size: Math.round(data.output.size / 1000),
+                        unit: 'kb'
+                    },
+                    compression: {
+                        ratio: data.output.ratio,
+                        percent: Math.round((1 - data.output.ratio) * 100)
+                    }
+                });
 
                 compressedFileCount ++;
                 totalSaving += data.input.size - data.output.size;
 
-                if (callback && validFileCount === compressedFileCount) {
-                    callback({
-                        saving: totalSaving / 1000
+                if (validFileCount === compressedFileCount) {
+                    time.end = new Date();
+
+                    event.emit('done', {
+                        saving: {
+                            size: Math.round(totalSaving / 1000),
+                            unit: 'kb'
+                        },
+                        time: {
+                            start: time.format(time.start.getTime()),
+                            end: time.format(time.end.getTime()),
+                            duration: time.format(time.duration()),
+                            unit: 'seconds'
+                        }
                     });
                 }
 
@@ -78,6 +137,8 @@ function walkDir(dir, apiKey, callback) {
             validFileCount ++;
         }
     });
+
+    return event;
 
 }
 
